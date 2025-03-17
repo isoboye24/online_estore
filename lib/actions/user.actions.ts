4c1ef38d3193f03f0993db06884014655355b0b2
isoboye24 <isobarguy4real@gmail.com>
2025-03-17 22:56:25 +0100

    I created the Next Auth API route, sign in and sign out actions

---
+++ lib/actions/user.actions.ts
@@ -0,0 +1,33 @@
+'use server';
+
+import { isRedirectError } from 'next/dist/client/components/redirect-error';
+import { signIn, signOut as authSignOut } from '@/auth';
+import { signInFormSchema } from '../validator';
+
+export async function signInWithCredentials(
+  prevState: unknown,
+  formData: FormData
+) {
+  try {
+    const email = formData.get('email')?.toString() || '';
+    const password = formData.get('password')?.toString() || '';
+
+    const user = signInFormSchema.parse({ email, password });
+
+    await signIn('credentials', user);
+
+    return { success: true, message: 'Signed in successfully' };
+  } catch (error) {
+    if (isRedirectError(error)) {
+      throw error;
+    }
+
+    console.error('Sign-in error:', error);
+
+    return { success: false, message: 'Invalid email or password' };
+  }
+}
+
+export async function signOutUser() {
+  await authSignOut();
+}
