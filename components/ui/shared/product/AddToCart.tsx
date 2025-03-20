9413716b58151b5eaaa00504c147aa9e1d0d453b
isoboye24 <isobarguy4real@gmail.com>
2025-03-21 00:36:07 +0100

    I implemented price calc, handled quantity and multiple product, and finally added it to the DB.

--- components/ui/shared/product/AddToCart.tsx
+++ components/ui/shared/product/AddToCart.tsx
@@ -18,7 +18,7 @@ const AddToCart = ({ item }: { item: CartItem }) => {
       return;
     }
 
-    toast.success(`${item.name} added to cart`, {
+    toast.success(res.message, {
       action: (
         <Button
           className="bg-primary text-white hover:bg-gray-800"
--- lib/actions/cart.actions.ts
+++ lib/actions/cart.actions.ts
@@ -1,11 +1,30 @@
 'use server';
 
 import { CartItem } from '@/types';
-import { convertToPlainJSObject, formatError } from '../utils';
+import { convertToPlainJSObject, formatError, round2 } from '../utils';
 import { cookies } from 'next/headers';
 import { auth } from '@/auth';
 import { prisma } from '@/db/prisma';
-import { cartItemSchema } from '../validator';
+import { cartItemSchema, insertCartSchema } from '../validator';
+import { z } from 'zod';
+import { revalidatePath } from 'next/cache';
+import { Prisma } from '@prisma/client';
+
+// Calculate cart price based on items
+const calcPrice = (items: z.infer<typeof cartItemSchema>[]) => {
+  const itemsPrice = round2(
+      items.reduce((acc, item) => acc + Number(item.price) * item.qty, 0)
+    ),
+    shippingPrice = round2(itemsPrice > 100 ? 0 : 10),
+    taxPrice = round2(0.15 * itemsPrice),
+    totalPrice = round2(itemsPrice + shippingPrice + taxPrice);
+  return {
+    itemsPrice: itemsPrice.toFixed(2),
+    shippingPrice: shippingPrice.toFixed(2),
+    taxPrice: taxPrice.toFixed(2),
+    totalPrice: totalPrice.toFixed(2),
+  };
+};
 
 export async function addItemToCart(data: CartItem) {
   try {
@@ -18,26 +37,72 @@ export async function addItemToCart(data: CartItem) {
     console.log('Session Object:', session);
     const userId = session?.user?.id ? (session.user.id as string) : undefined;
 
-    // const cart = await getMyCart();
+    const cart = await getMyCart();
 
     const item = cartItemSchema.parse(data);
 
     const product = await prisma.product.findFirst({
       where: { id: item.productId },
     });
+    if (!product) {
+      throw new Error('Product not found');
+    }
 
-    // TESTING
-    console.log({
-      'Session Cart Id': sessionCartId,
-      'User ID': userId,
-      'Item Requested': item,
-      Product: product,
-    });
+    if (!cart) {
+      const newCart = insertCartSchema.parse({
+        userId: userId,
+        items: [item],
+        sessionCartId: sessionCartId,
+        ...calcPrice([item]),
+      });
 
-    return {
-      success: true,
-      message: 'Item added to cart',
-    };
+      // Add to DB
+      await prisma.cart.create({
+        data: newCart,
+      });
+
+      // revalidate product page
+      revalidatePath(`/product/${product.slug}`);
+
+      return {
+        success: true,
+        message: `${product.name} added to cart`,
+      };
+    } else {
+      const existItem = (cart.items as CartItem[]).find(
+        (x) => x.productId === item.productId
+      );
+
+      if (existItem) {
+        if (product.stock < existItem.qty + 1) {
+          throw new Error('Not enough stock');
+        }
+
+        (cart.items as CartItem[]).find(
+          (x) => x.productId === item.productId
+        )!.qty = existItem.qty + 1;
+      } else {
+        if (product.stock < 1) throw new Error('Not enough stock');
+        cart.items.push(item);
+      }
+
+      await prisma.cart.update({
+        where: { id: cart.id },
+        data: {
+          items: cart.items as Prisma.CartUpdateitemsInput[],
+          ...calcPrice(cart.items as CartItem[]),
+        },
+      });
+
+      revalidatePath(`/product/${product.slug}`);
+
+      return {
+        success: true,
+        message: `${product.name} ${
+          existItem ? 'updated in' : 'added to'
+        } cart successfully`,
+      };
+    }
   } catch (error) {
     return {
       success: false,
