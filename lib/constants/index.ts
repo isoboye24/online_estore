6a0c1575da6f25f4d85275b5f4476644c404d29e
isoboye24 <isobarguy4real@gmail.com>
2025-03-22 02:07:19 +0100

    I created cart page and implemented ShadCN Table and displayed the items in the cart.

--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -8,8 +8,8 @@ export const LATEST_PRODUCTS_LIMIT =
   Number(process.env.LATEST_PRODUCTS_LIMIT) || 4;
 
 export const signInDefaultValues = {
-  email: 'isoboyedanobu@gmail.com',
-  password: '123',
+  email: '',
+  password: '',
 };
 
 export const signUpDefaultValues = {
