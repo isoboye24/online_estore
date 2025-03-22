8d188202e65954ea1758bbb7f171c329be3aa78a
isoboye24 <isobarguy4real@gmail.com>
2025-03-22 01:23:59 +0100

    I implemented smooth Loader of the add-to-cart with useTransition Hook.

--- components/ui/shared/product/AddToCart.tsx
+++ components/ui/shared/product/AddToCart.tsx
@@ -4,43 +4,48 @@ import { useRouter } from 'next/navigation';
 import { Cart, CartItem } from '@/types';
 import { addItemToCart, removeItemFromCart } from '@/lib/actions/cart.actions';
 import { toast } from 'sonner';
-import { Plus, Minus } from 'lucide-react';
+import { Plus, Minus, Loader } from 'lucide-react';
+import { useTransition } from 'react';
 
 const AddToCart = ({ cart, item }: { cart?: Cart; item: CartItem }) => {
   const router = useRouter();
+  const [isPending, startTransition] = useTransition();
 
   const handleAddToCart = async () => {
-    const res = await addItemToCart(item);
+    startTransition(async () => {
+      const res = await addItemToCart(item);
 
-    if (!res.success) {
-      toast.error(res.message);
+      if (!res.success) {
+        toast.error(res.message);
 
-      return;
-    }
+        return;
+      }
 
-    toast.success(res.message, {
-      action: (
-        <Button
-          className="bg-primary text-white hover:bg-gray-800"
-          onClick={() => router.push('/cart')}
-        >
-          Go to cart
-        </Button>
-      ),
+      toast.success(res.message, {
+        action: (
+          <Button
+            className="bg-primary text-white hover:bg-gray-800"
+            onClick={() => router.push('/cart')}
+          >
+            Go to cart
+          </Button>
+        ),
+      });
     });
   };
 
-  // Remove item from cart
   const handleRemoveFromCart = async () => {
-    const res = await removeItemFromCart(item.productId);
+    startTransition(async () => {
+      const res = await removeItemFromCart(item.productId);
 
-    if (res.success) {
-      toast.success(res.message);
-    } else {
-      toast.error(res.message);
-    }
+      if (res.success) {
+        toast.success(res.message);
+      } else {
+        toast.error(res.message);
+      }
 
-    return;
+      return;
+    });
   };
 
   const existItem =
@@ -49,16 +54,28 @@ const AddToCart = ({ cart, item }: { cart?: Cart; item: CartItem }) => {
   return existItem ? (
     <div>
       <Button type="button" variant="outline" onClick={handleRemoveFromCart}>
-        <Minus className="w-4 h-4" />
+        {isPending ? (
+          <Loader className="w-4 h-4  animate-spin" />
+        ) : (
+          <Minus className="w-4 h-4" />
+        )}
       </Button>
       <span className="px-2">{existItem.qty}</span>
       <Button type="button" variant="outline" onClick={handleAddToCart}>
-        <Plus className="w-4 h-4" />
+        {isPending ? (
+          <Loader className="w-4 h-4 animate-spin" />
+        ) : (
+          <Plus className="w-4 h-4" />
+        )}
       </Button>
     </div>
   ) : (
     <Button className="w-full" type="button" onClick={handleAddToCart}>
-      <Plus className="w-4 h-4" />
+      {isPending ? (
+        <Loader className="w-4 h-4 animate-spin" />
+      ) : (
+        <Plus className="w-4 h-4" />
+      )}
       Add to cart
     </Button>
   );
