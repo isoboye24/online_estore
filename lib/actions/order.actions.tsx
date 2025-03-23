eed59977211e4fe55c3e7d1e1508976de59647db
isoboye24 <isobarguy4real@gmail.com>
2025-03-23 21:22:16 +0100

    I created the order page and its action.

--- lib/actions/order.actions.tsx
+++ lib/actions/order.actions.tsx
@@ -1,7 +1,7 @@
 'use server';
 
 import { isRedirectError } from 'next/dist/client/components/redirect-error';
-import { formatError } from '../utils';
+import { formatError, convertToPlainJSObject } from '../utils';
 import { auth } from '@/auth';
 import { getMyCart } from './cart.actions';
 import { getUserById } from './user.actions';
@@ -90,4 +90,17 @@ export async function createOrder() {
     if (isRedirectError(error)) throw error;
     return { success: false, message: formatError(error) };
   }
+}
+
+export async function getOrderById(orderId: string) {
+  const data = await prisma.order.findFirst({
+    where: {
+      id: orderId,
+    },
+    include: {
+      orderItems: true,
+      user: { select: { name: true, email: true } },
+    },
+  });
+  return convertToPlainJSObject(data);
 }
