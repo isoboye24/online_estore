cb9c04f4ad392fc36deb4fc3698c8644eedee9d8
isoboye24 <isobarguy4real@gmail.com>
2025-03-28 23:58:13 +0100

    I implemented admin user search

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -16,6 +16,7 @@ import { ShippingAddress } from '@/types';
 import { z } from 'zod';
 import { PAGE_SIZE } from '@/lib/constants';
 import { revalidatePath } from 'next/cache';
+import { Prisma } from '@prisma/client';
 
 export async function signInWithCredentials(
   prevState: unknown,
@@ -177,14 +178,30 @@ export async function updateProfile(user: { name: string; email: string }) {
 }
 
 // Get all users
+
 export async function getAllUsers({
   limit = PAGE_SIZE,
   page,
+  query,
 }: {
   limit?: number;
   page: number;
+  query: string;
 }) {
+  const queryFilter: Prisma.UserWhereInput =
+    query && query !== 'all'
+      ? {
+          name: {
+            contains: query,
+            mode: 'insensitive',
+          } as Prisma.StringFilter,
+        }
+      : {};
+
   const data = await prisma.user.findMany({
+    where: {
+      ...queryFilter,
+    },
     orderBy: { createdAt: 'desc' },
     take: limit,
     skip: (page - 1) * limit,
