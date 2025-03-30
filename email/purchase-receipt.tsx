ed5754fe38b9d83459cdf22589d38bd1daf6d9f5
isoboye24 <isobarguy4real@gmail.com>
2025-03-31 00:50:40 +0200

    I implemented preview email in Browser with React.

--- email/purchase-receipt.tsx
+++ email/purchase-receipt.tsx
@@ -14,12 +14,55 @@ import {
 } from '@react-email/components';
 import { formatCurrency } from '@/lib/utils';
 import { Order } from '@/types';
-// import sampleData from '@/db/sample-data';
+import sampleData from '@/db/sample-data';
 // require('dotenv').config();
 
-// type OrderInformationProps = {
-//   order: Order;
-// };
+type OrderInformationProps = {
+  order: Order;
+};
+
+PurchaseReceiptEmail.PreviewProps = {
+  order: {
+    id: crypto.randomUUID(),
+    userId: '123',
+    user: {
+      name: 'John Doe',
+      email: 'bS8Rn@example.com',
+    },
+    paymentMethod: 'Stripe',
+    shippingAddress: {
+      fullName: 'John Doe',
+      streetAddress: '123 Main St',
+      city: 'New York',
+      postalCode: '10001',
+      country: 'US',
+    },
+    createdAt: new Date(),
+    totalPrice: '100',
+    taxPrice: '10',
+    shippingPrice: '10',
+    itemsPrice: '80',
+    orderItems: sampleData.products.map((x) => ({
+      name: x.name,
+      orderId: '123',
+      productId: '123',
+      slug: x.slug,
+      qty: x.stock,
+      image: x.images[0],
+      price: x.price.toString(),
+    })),
+    isDelivered: true,
+    deliveredAt: new Date(),
+    isPaid: true,
+    paidAt: new Date(),
+    paymentResult: {
+      id: '123',
+      status: 'succeeded',
+      pricePaid: '12',
+      email_address: 'bS8Rn@example.com',
+    },
+  },
+} satisfies OrderInformationProps;
 
 const dateFormatter = new Intl.DateTimeFormat('en', { dateStyle: 'medium' });
 
